02539b4a5eade050fdf878913473c8c2b74cbc75
asishricky <asishisrael@email.com>
2025-08-04 10:46:14 +0530

    feat: Add initial project files

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/my-first-git-project">
+    <Router basename="/Ecommerce-git">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
